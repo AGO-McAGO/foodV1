a4740cae1efc7a08197689aabf4e83d71d616078
AGO McAGO <agomcago@gmail.com>
2022-11-01 14:11:15 +0100

    adding one line title functionality.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,17 +1,44 @@
 
 import { elements } from "./base"; // all DOM elements imported.
 
+
+
+
 export const getInput = () => elements.searchInput.value; // to get the value the user entered for a search.
+
 
 // to clear the search input field.
 export const clearInputField = () => {
     elements.searchInput.value = ""; // set the value equal to nothing (i.e. empty).
 };
 
+
 // to clear the results from the previous search.
 export const clearResults = () => {
     elements.resultsList.innerHTML = ""; // set to nothing, i.e. the li elements/tags will be deleted.
 };
+
+
+// to shorten the title with dots, if it's too long to be on one line.
+const limitRecipeTitle = (title, limit = 15) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        /*
+        to split the title by a space. The callback function is the 1st argument of the "reduce()" method with "acclt" stands for accumulator and
+        "currV" stands for current value as parameters. The 2nd argument of the "reduce()" method is initial value of the accumulator which is "0".
+        */
+        title.split(" ").reduce( (acclt, currV) => {
+            if (acclt + currV.length <= limit) {
+                newTitle.push(currV);
+            }
+            return acclt + currV.length;
+        }, 0 );
+        return `${newTitle.join(" ")} ....`;
+    }
+    return title;
+
+};
+
 
 // to display only one recipe.
 const renderRecipe = recipi => {
@@ -23,7 +50,7 @@ const renderRecipe = recipi => {
                             <img src="${recipi.image_url}" alt="${recipi.title}">
                         </figure>
                         <div class="results__data">
-                            <h4 class="results__name">${recipi.title}</h4>
+                            <h4 class="results__name">${limitRecipeTitle(recipi.title)}</h4>
                             <p class="results__author">${recipi.publisher}</p>
                         </div>
                     </a>
@@ -32,6 +59,7 @@ const renderRecipe = recipi => {
     // to insert the template into the webpage.
     elements.resultsList.insertAdjacentHTML("beforeend", recipeMarkup);
 };
+
 
 // for all recipes.
 export const renderResults = recipes => {
